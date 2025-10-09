c25b8d054d26017000e821af987f2d6c97b7975e
«Любовь <anissa310@yandex.ru>
2025-10-09 15:33:51 +0300

    добавление функции gotoScreen_log

--- app.py
+++ app.py
@@ -3,11 +3,24 @@ from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLa
 from PyQt6.QtGui import QPixmap #для картинок
 from PyQt6.QtCore import Qt
 
+class Log_Window(QWidget):
+    def  __init__(self):
+        super().__init__()
+        self.initializeUI()
+    def initializeUI(self):#задача базовых настроек приложения
+        self.setGeometry(600, 200, 800, 600) #600, 200 - отступ при создании, 800, 600 - размер окна
+        self.setWindowTitle("регистрация")
+        self.setUpLog_Window()
+        self.show()
+    def setUpLog_Window(self):
+        pass
+
 class First_Window(QWidget):
     def __init__(self):
         super().__init__()
         self.screen_vhod = QWidget()
         self.screen_kon = QWidget()
+        self.screen_log = QWidget()
         self.initializeUI()
 
     def initializeUI(self):#задача базовых настроек приложения
@@ -29,6 +42,7 @@ class First_Window(QWidget):
         self.ot_button = QPushButton("войти", self)
         self.ot_button.setEnabled(False)
         self.log_button = QPushButton("зарегистрироваться", self)
+        self.log_button.clicked.connect(self.gotoScreen_log)
 
 
         log_h_box = QHBoxLayout()
@@ -45,6 +59,7 @@ class First_Window(QWidget):
         self.stacked_layout = QStackedLayout()
         self.stacked_layout.addWidget(self.screen_vhod)
         self.stacked_layout.addWidget(self.screen_kon)
+        self.stacked_layout.addWidget(self.screen_log)
 
         self.setLayout(main_v_box)
 
@@ -57,6 +72,10 @@ class First_Window(QWidget):
     
     def gotoScreen_kon(self):#переход на новую страницу
         self.stacked_layout.setCurrentWidget(self.screen_kon)
+
+    def gotoScreen_log(self):
+        self.screen_log = Log_Window()
+        self.screen_log.show()
 
 app = QApplication(sys.argv)
 window = First_Window()
