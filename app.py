d83fd7f9ed8617c4dc19b4cd5bcaaeae192ea526
lvrv_pop <82032801+Fly-Agaric-Fly@users.noreply.github.com>
2025-10-26 20:58:15 +0300

    Кнопка удаления 2.0

--- app.py
+++ app.py
@@ -243,12 +243,30 @@ class Main_Window(QMainWindow):  # окно с выбором предмета, 
         link = self.ui.table.item(row, 1).text()
         date = self.ui.table.item(row, 2).text()
 
-        QMessageBox.setStyleSheet("QLabel{ color: white}")
-        reply = QMessageBox.question(
-    self, 'Удаление', f'Удалить конспект по предмету "{subject}"?',
-    QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
+    #Настройка цвета для кнопки удаления
+    def ask_delete(self):
+        msg = QMessageBox(self)
+        msg.setWindowTitle("Удаление")
+        msg.setText(f'Удалить конспект по предмету "{subject}"?')
+        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
 
-)
+        msg.setStyleSheet("""
+            QMessageBox {
+                background-color: #121212;
+            }
+            QPushButton {
+                background-color: #333;
+                color: white;
+                border-radius: 6px;
+                padding: 6px;
+            }
+            QPushButton:hover {
+                background-color: #555;
+            }
+        """)
+
+        msg.exec()
+        return
         
         if reply == QMessageBox.StandardButton.Yes:
             delete_file(USER_ID, subject, name_file, link, date)
